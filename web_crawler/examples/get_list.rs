9a7fb5dca7df54e5331cb3250d330a5ca1143c7e
Archibald Neil MacDonald <1823745+aneilmac@users.noreply.github.com>
2021-02-22 01:36:53 +0000

    Update get_list.rs

--- web_crawler/examples/get_list.rs
+++ web_crawler/examples/get_list.rs
@@ -2,7 +2,7 @@ use reqwest::{Result, Url};
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    let url = Url::parse("https://www.reddit.com").unwrap();
+    let url = Url::parse("https://www.enhance.com").unwrap();
     println!("Crawling for {}:", url);
 
     let list = web_crawler_lib::unique_url_list(url.clone()).await?;
